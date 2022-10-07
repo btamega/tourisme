98d383753d4ba3d58ee384314c1d48f66f280cab
kannoufa <fkannoufa@gmail.com>
2022-10-07 19:27:25 +0100

    fill database

---
+++ src/main/java/Bibliotheque/Bibliotheque.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Bibliotheque;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author dell
+ */
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Bibliotheque {
+    private String name;
+    private String localisation;
+    private String siteweb;
+    private String telephone;
+    private String email;
+    
+}
--- src/main/java/bean/RessourceBean.java
+++ src/main/java/bean/RessourceBean.java
@@ -4,6 +4,7 @@
  */
 package bean;
 
+import Bibliotheque.Bibliotheque;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
@@ -55,6 +56,69 @@ public class RessourceBean {
         System.out.println(sitesList.size());
         return sitesList;
     }
+    /*
+    public List<Pharmacie> getPharmacies(){
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017/");
+        MongoDatabase database = mongoClient.getDatabase("tourisme");
+        MongoCollection<Document> collection = database.getCollection("ressources");
+        
+        FindIterable<Document> docs = collection.find();
+        List<Pharmacie> pharmacies = new ArrayList<>();
+        
+        for( Document doc :docs){
+            
+            
+            Document ressources = (Document) doc.get("ressources");
+            List<Document> ressource = (List<Document>) ressources.get("ressource");
+             
+            
+               // List<Document> pharmacies = (List<Document>) ressource.get(0).get("pharmacies");
+                for (Document document1 : pharmacies) {
+                    Pharmacie pharmacie = new Pharmacie();
+                    pharmacie.setName((String) document1.get("name"));
+                    System.out.println(" -------> name = "+(String) document1.get("name"));
+                }
+        }
+        
+        
+        return pharmacies;
+    }
+    
+*/
+    public List<Bibliotheque> getBibliotheques(){
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017/");
+        MongoDatabase database = mongoClient.getDatabase("tourisme");
+        MongoCollection<Document> collection = database.getCollection("ressources");
+        
+        FindIterable<Document> docs = collection.find();
+        List<Bibliotheque> bibliotheques = new ArrayList<>();
+        
+        for( Document doc :docs){
+            
+            
+            Document ressources = (Document) doc.get("ressources");
+            List<Document> ressource = (List<Document>) ressources.get("ressource");
+             
+            
+                List<Document> bibliothequeL = (List<Document>) ressource.get(3).get("bibliotheque");
+                for (Document document1 : bibliothequeL) {
+                    Bibliotheque bibliotheque = new Bibliotheque();
+                    bibliotheque.setName((String) document1.get("name"));
+                    bibliotheque.setLocalisation((String) document1.get("localisation"));
+                    bibliotheque.setSiteweb((String) document1.get("siteweb"));
+                    bibliotheque.setTelephone((String) document1.get("telephone"));
+                    bibliotheque.setEmail((String) document1.get("email"));
+                    bibliotheques.add(bibliotheque);
+
+                    System.out.println(" -------> name = "+(String) document1.get("name"));
+                }
+        }
+        
+        
+        return bibliotheques;
+    }
+    
+    
     public void createRess(){
         
     }
